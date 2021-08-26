4f4ca972cb0ebced8ed435c80d22ae03adaae422
Ryan Raasch <rmr@silicom.dk>
2021-08-26 10:15:31 +0000

    Enable internal container registry

--- first.sh
+++ first.sh
@@ -352,5 +352,6 @@ cp -av $KUBECONFIG ~/.kube/
 sleep 60
 
 $OC get csr -o name | xargs oc adm certificate approve
-
+# https://docs.openshift.com/container-platform/4.8/registry/configuring-registry-operator.html
+$OC patch config cluster -n openshift-image-registry --type merge --patch '{"spec": { "managementState": "Managed"}}'
 # $OC apply -f ${BASE}/files/nfd-daemonset.yaml
